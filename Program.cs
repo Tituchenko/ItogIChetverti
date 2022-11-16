b6e546ae4cbf354976e7b324a1957efbc992d8d2
Ilya Tituchenko <titu4enko@mail.ru>
2022-11-16 14:01:00 +0300

    Create first version GetArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿GetArray();
+
+void GetArray ()
+{
+    Console.WriteLine("Введите исходный массив:");
+    Console.WriteLine("1. Просто нажав Enter, пустой ввод - тогда массив буде сформирован случайным образом из тестовых к заданию.");
+    Console.WriteLine("2. Введите числе элементов (цифра), тога далее будет поэлементно запрошен массив.");
+    Console.WriteLine("3. Введите полностью массив в строке, используя ';' как разделитель элементов");
+    string inputUser=Console.ReadLine()!;
+    if (inputUser==string.Empty) Console.WriteLine("пустой ввод");
+}
