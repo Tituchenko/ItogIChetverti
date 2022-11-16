cbc5d7415d49de0e8d563bc1820a4accbe0ed039
Ilya Tituchenko <titu4enko@mail.ru>
2022-11-16 16:18:54 +0300

    Engine solved

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string [] test={"dff","43535","-2"};
+Console.WriteLine(String.Join(";",GetArratLessSymbol(test,3)));
+
+string [] GetArratLessSymbol (string [] arr,int numSymb){
+    string [] temp=new string [arr.Length];
+    int count=0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length<=numSymb) {
+            temp[count]=arr[i];
+            count++;
+        }
+        
+    }
+    string [] ret=new string [count];
+    for (int i = 0; i < count; i++)
+    {
+        ret[i]=temp[i];
+    }
+    return ret;
+
+}
