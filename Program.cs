df97a104c381eec987a3dcc338cb2a43e706cb6e
Ilya Tituchenko <titu4enko@mail.ru>
2022-11-16 16:47:58 +0300

    Output finish

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] a={"1","4","dfdfdf"};
+string [] b={"1","4"};
+PrintResult (a,b);
+
+void PrintResult (string [] arr, string [] res) {
+    PrintArray (arr);
+    Console.Write ("->");
+    PrintArray (res);
+}
+
+void PrintArray (string [] array) {
+    Console.Write ("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write ('"');
+        Console.Write ($"{array[i]}"); 
+        Console.Write ('"');
+        if (i<array.Length-1) Console.Write (","); 
+    }
+    Console.Write ("]");
+}
